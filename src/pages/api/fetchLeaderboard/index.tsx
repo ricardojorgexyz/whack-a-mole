884e8fd23bff605c70be7288c4554b5169755fdd
ricardojorgeio <ricardojorgexyz@gmail.com>
2023-05-12 12:58:04 +0100

    chore: add note

--- src/pages/api/fetchLeaderboard/index.tsx
+++ src/pages/api/fetchLeaderboard/index.tsx
@@ -2,6 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import { token, endpoint } from '../../../cfg/airtable';
 
+/*
+ *
+ * NOTE: I'm using a table view that is already sorted to
+ * the top scores and I'm limiting the results to 10 on
+ * the end point
+ *
+ * 'https://api.airtable.com/v0/{tableId}/game-table?maxRecords=10&view=sorted
+ *
+ */
+
 export default function fetchLeaderboard(
   _req: NextApiRequest,
   res: NextApiResponse,
