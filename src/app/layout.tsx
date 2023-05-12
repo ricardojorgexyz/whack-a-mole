c8cd5f9f90f212141d506d6e48efc68e6d4f2fb5
ricardojorgeio <ricardojorgexyz@gmail.com>
2023-05-12 12:38:50 +0100

    feat: add fonts and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,19 @@
+'use client';
+
 import React from 'react';
+import { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
+export const metadata: Metadata = {
+  title: 'Whack-a-mole',
+  viewport: 'initial-scale=1, width=device-width',
+};
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main className={roboto.className}>{children}</main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
 import React from 'react';
+import { Button } from '@mui/material';
 
 export default function Home() {
-  return <main>HOME</main>;
+  return (
+    <div>
+      <div>HOME</div>
+      <div>
+        <Button variant="contained">Beep</Button>
+      </div>
+    </div>
+  );
 }
