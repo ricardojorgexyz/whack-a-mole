febfaf8ae7256b4ed856f8664291a9019c0fcab1
ricardojorgeio <ricardojorgexyz@gmail.com>
2023-05-12 20:55:39 +0100

    chore: game view wip

--- src/components/GameView/index.tsx
+++ src/components/GameView/index.tsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { ScreenOpts } from '@/types';
+import { Box, Grid } from '@mui/material';
+import { useInterval } from '@/hooks/useInterval';
+import { GAME_DURATION } from '@/cfg/game';
+import { rng } from '@/scripts/rng';
+import { Target } from './target';
 
-export const GameView = () => {
-  return <>Game</>;
+interface SubProps {
+  screenSet: React.Dispatch<React.SetStateAction<ScreenOpts>>;
+  parseDiff: {
+    int: number;
+    reward: number;
+  };
+  incrementScore: () => void;
+}
+
+export const GameView = ({
+  screenSet,
+  parseDiff,
+  incrementScore,
+}: SubProps) => {
+  const [activeTarget, activeTargetSet] = useState<number>(rng(1, 12));
+
+  return (
+    <Box>
+      <Grid
+        container
+        justifyContent={'center'}
+        spacing={1}
+        alignItems={'center'}
+      >
+        {/* NOTE: coding this part at the end of the day on friday with time running out, at least I tried to make this hack somewhat tasteful */}
+        {'😂'
+          .padEnd(12, '🤣')
+          .split('')
+          .map((_val, i) => {
+            return (
+              <Grid item xs={3} key={`${_val}-${i}`} py={9}>
+                <Target active={i === activeTarget} />
+              </Grid>
+            );
+          })}
+      </Grid>
+    </Box>
+  );
 };
--- src/components/HomeView/index.tsx
+++ src/components/HomeView/index.tsx
@@ -1,17 +1,63 @@
 'use client';
 
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { Box } from '@mui/material';
 import { ScreenOpts } from '@/types';
+import {
+  GAME_DIFF_EASY_INTERVAL,
+  GAME_DIFF_EASY_SCORE,
+  GAME_DIFF_HARD_INTERVAL,
+  GAME_DIFF_HARD_SCORE,
+  GAME_DIFF_MED_INTERVAL,
+  GAME_DIFF_MED_SCORE,
+} from '@/cfg/game';
+import { useSettings } from '@/hooks/useSettings';
 import { HomeWelcome } from './welcome';
+import { GameView } from '../GameView';
 
 export const HomeView = () => {
-  const [screen, screenSet] = useState<ScreenOpts>('no-game');
+  const { diff } = useSettings();
+
+  const [screen, screenSet] = useState<ScreenOpts>('in-game');
+  const [score, scoreSet] = useState<number>(0);
+
+  const parseDiff = useMemo(() => {
+    switch (diff) {
+      case 'hard':
+        return {
+          int: GAME_DIFF_HARD_INTERVAL,
+          reward: GAME_DIFF_HARD_SCORE,
+        };
+
+      case 'med':
+        return {
+          int: GAME_DIFF_MED_INTERVAL,
+          reward: GAME_DIFF_MED_SCORE,
+        };
+
+      case 'easy':
+      default:
+        return {
+          int: GAME_DIFF_EASY_INTERVAL,
+          reward: GAME_DIFF_EASY_SCORE,
+        };
+    }
+  }, [diff]);
+
+  const incrementScore = useCallback(() => {
+    scoreSet(score + parseDiff.reward);
+  }, [parseDiff, score, scoreSet]);
 
   return (
     <Box>
       {screen === 'no-game' && <HomeWelcome screenSet={screenSet} />}
-      {screen === 'in-game' && <HomeWelcome screenSet={screenSet} />}
+      {screen === 'in-game' && (
+        <GameView
+          incrementScore={incrementScore}
+          parseDiff={parseDiff}
+          screenSet={screenSet}
+        />
+      )}
       {screen === 'end-game' && <HomeWelcome screenSet={screenSet} />}
     </Box>
   );
